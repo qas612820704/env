7bcda27f5b7ce5025689602ea19d1faa6056b28a
qas612820704@gmail.com <qas612820704@gmail.com>
2017-12-13 01:15:21 +0800

    Change ln to symbolic, avoid file untrackable when save file

--- install.sh
+++ install.sh
@@ -3,27 +3,27 @@
 ## Vim Setting
 
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-ln ./.vimrc ~
+ln -s ./.vimrc ~
 vim +PluginInstall +qall
 
 ## Zshrc
 
 git clone https://github.com/olivierverdier/zsh-git-prompt.git ~/zsh-git-prompt
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/zsh-syntax-highlighting
-ln ./.zshrc ~
+ln -s ./.zshrc ~
 
 ## Alias file
 
-ln ./.aliases ~
+ln -s ./.aliases ~
 
 
 ## awesome config
 
 mkdir -p ~/.config/awesome/
-ln ./rc.lua ~/.config/awesome/
+ln -s ./rc.lua ~/.config/awesome/
 
 
 ## tmux conf
 
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-ln ./.tmux.conf ~
+ln -s ./.tmux.conf ~
