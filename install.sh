0545c4bfffcb32fcf6c85d33414954acdbacd057
Lego <qas612820704@gmail.com>
2019-11-08 12:01:17 +0800

    Update install.sh
    Relace ~ to $HOME since bash4.4

--- install.sh
+++ install.sh
@@ -2,28 +2,28 @@
 
 ## Vim Setting
 
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-ln -s ./.vimrc ~
+git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
+ln -s ./.vimrc $HOME
 vim +PluginInstall +qall
 
 ## Zshrc
 
-git clone https://github.com/olivierverdier/zsh-git-prompt.git ~/zsh-git-prompt
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/zsh-syntax-highlighting
-ln -s ./.zshrc ~
+git clone https://github.com/olivierverdier/zsh-git-prompt.git $HOME/zsh-git-prompt
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/zsh-syntax-highlighting
+ln -s ./.zshrc $HOME
 
 ## Alias file
 
-ln -s ./.aliases ~
+ln -s ./.aliases $HOME
 
 
 ## awesome config
 
-mkdir -p ~/.config/awesome/
-ln -s ./rc.lua ~/.config/awesome/
+mkdir -p $HOME/.config/awesome/
+ln -s ./rc.lua $HOME/.config/awesome/
 
 
 ## tmux conf
 
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-ln -s ./.tmux.conf ~
+git clone https://github.com/tmux-plugins/tpm $HOME/.tmux/plugins/tpm
+ln -s ./.tmux.conf $HOME
